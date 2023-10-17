6813e1f636e8e74cf0be3ea378e66fb2b11cc4ef
Christian Sucuzhanay Arevalo <christiansucuzhanayarevalo@Christians-MacBook-Air.local>
2023-10-17 07:14:28 +0200

    Just creating Dockerfile

---
+++ pythonProject/main.py
@@ -0,0 +1,17 @@
+# My first python program
+name = input('Who are you ?')
+print('Welcome '+ name + ' to my first program')
+current_age = int(input('Hey enter your age ?'))
+legal_age = 18
+running_out = legal_age-current_age
+
+for i in range(running_out):
+    print(name + ' estas a ', i+1 ,' año de irte de casa')
+
+
+
+
+
+
+
+
